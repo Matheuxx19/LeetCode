c98a87177daffb4f2dd84fb3dc0fafa104faeb48
Leo <Leonardoginmesquita@gmail.com>
2024-11-04 18:18:31 -0300

    0009-PALINDROME-NUMBER Stats: Time: 11 ms (5.72%), Space: 9.1 MB (6.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -4,8 +4,6 @@
         sprintf(str, "%d", x);
         int l = strlen(str);
         for(i=0;i<l/2;i++){
-            printf("%d, %d\n",i,l-i-1);
-            printf("%c, %c\n",str[i],str[l-i-1]);
             if(str[i]!=str[l-i-1]){
                 return false;
             }
